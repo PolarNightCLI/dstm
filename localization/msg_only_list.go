d9794daefecfeaa4bc85b2d46d9bb641ad5af4ca
yechentide <yechentide@gmail.com>
2022-05-21 20:00:10 +0900

    feat: 新的本地化字符串

--- localization/msg_only_list.go
+++ localization/msg_only_list.go
@@ -27,4 +27,28 @@ var msgOnlyList = map[string]func() *i18n.LocalizeConfig{
 			},
 		}
 	},
+	"_no_running_shards": func() *i18n.LocalizeConfig {
+		return &i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "_no_running_shards",
+				Other: "no running shards",
+			},
+		}
+	},
+	"_tip01": func() *i18n.LocalizeConfig {
+		return &i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "_tip01",
+				Other: "Server not showing in server list?   -->   Check port and firewall!",
+			},
+		}
+	},
+	"_tip02": func() *i18n.LocalizeConfig {
+		return &i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "_tip02",
+				Other: "Got a update when start game?        -->   Update server side too!",
+			},
+		}
+	},
 }
